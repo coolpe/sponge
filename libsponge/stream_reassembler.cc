e7d5e8ae061331ba0b0be55d9259cbc3f112c9a9
gole <b_igboss@163.com>
2021-12-07 16:04:15 +0800

    lab1 finished

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -7,9 +7,6 @@
 
 // You will need to add private members to the class declaration in `stream_reassembler.hh`
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
@@ -18,9 +15,35 @@ StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity),
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    size_t data_size = data.size();
+    if (_capacity < data_size){
+        return;
+    }
+    _capacity -=data_size;
+    if (eof) {
+        _eof_index = index;
+    }
+    if (_next_index == index || _output.remaining_capacity() >= data_size) {
+        _output.write(data);
+        _next_index++;
+    } else{
+        if (!_una_substring[index]){
+            _una_substring[index] = &data;
+            _una_bytes += data_size;
+        }
+    }
+    while (_una_substring[_next_index]){
+        if (_output.remaining_capacity() < _una_substring[_next_index]->size()) {
+            break;
+        }
+        _output.write(*_una_substring[_next_index]);
+        _una_substring[_next_index] = NULL;
+        _next_index++;
+        _una_bytes -=data_size;
+    }
+
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return _una_bytes; }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return _una_bytes == 0; }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,16 +5,20 @@
 
 #include <cstdint>
 #include <string>
+#include <vector>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
-
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
-
+    size_t _una_bytes = 0;
+    uint64_t _next_index = 0;
+    uint64_t _eof_index = 0;
+    std::vector<const std::string *> _una_substring;
+    bool _empty = false;
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
     //! \note This capacity limits both the bytes that have been reassembled,
