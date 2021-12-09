ed9035184522856f87e32d8d089688ba664ecf4a
gole <b_igboss@163.com>
2021-12-09 18:51:36 +0800

    lab2 wrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -5,17 +5,14 @@
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    n += isn.raw_value();
+    return WrappingInt32{uint32_t(n&0xffffffff)};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +26,22 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint32_t offset = n.raw_value() - isn.raw_value();
+    uint64_t mask = static_cast<uint64_t>(UINT32_MAX) << 32;
+    uint64_t top32 = checkpoint & mask;
+    uint64_t num1 = (top32 - (1ul << 32)) + offset;
+    uint64_t abs1 = checkpoint - num1;
+    uint64_t num2 = top32 + offset;
+    uint64_t abs2 = (checkpoint > num2) ? (checkpoint - num2) : (num2 - checkpoint);
+    uint64_t num3 = (top32 + (1ul << 32)) + offset;
+    uint64_t abs3 = num3 - checkpoint;
+    if (top32 == 0) {
+        return (abs2 < abs3) ? num2 : num3;
+    }
+
+    if (abs1 < abs2) {
+        return (abs1 < abs3) ? num1 : num3;
+    }
+
+    return (abs2 < abs3) ? num2 : num3;
 }
